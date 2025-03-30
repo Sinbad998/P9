e37475e5dc62bcdf8119874d02b48a41c8e9e1a7
younes oublal <oublal.youness72@gmail.com>
2025-03-30 19:25:25 +0200

    p9

---
+++ script.js
@@ -0,0 +1,26 @@
+/*nav-bar**/ 
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a');
+
+menuIcon.onclick = () => {
+  navLinks.classList.toggle('active');
+}
+
+links.forEach(link => {
+  link.onclick = () => {
+    navLinks.classList.remove('active');
+  };
+});
+
+/*btn-github*/
+const githubBtn = document.querySelectorAll('.github-repo-btn')
+githubBtn.forEach(function(button) {
+  button.onclick = function() {
+      window.open(button.getAttribute('data-url'), '_blank');
+  };
+});
+
+
+
+/*Form*/
